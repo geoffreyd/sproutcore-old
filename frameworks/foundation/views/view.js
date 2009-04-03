48605963abfda58998797f15d770eadb9c19127c
Mike Ball <mike.ball3@gmail.com>
2009-04-03 16:43:25 -0400

    only materialize records on demand and apply layout styles when rendering

--- frameworks/foundation/views/view.js
+++ frameworks/foundation/views/view.js
@@ -1928,7 +1928,7 @@ SC.View = SC.Object.extend(SC.Responder, SC.DelegateSupport,
     @test in layoutChildViews
   */
   renderLayout: function(context, firstTime) {
-    console.error('renderLayout is not implemented');
+    context.addStyle(this.get('layoutStyle'));
   }
   
 });
